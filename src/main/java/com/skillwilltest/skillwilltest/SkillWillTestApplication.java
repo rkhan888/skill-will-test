6f1d01346e6b1bc808a223cdb45c1538b5116415
rumman <us.rkhan129@gmail.com>
2021-02-09 18:08:14 -0500

    initial commit

---
+++ src/main/java/com/skillwilltest/skillwilltest/SkillWillTestApplication.java
@@ -0,0 +1,13 @@
+package com.skillwilltest.skillwilltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillWillTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillWillTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillwilltest/skillwilltest/SkillWillTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillwilltest.skillwilltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillWillTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
