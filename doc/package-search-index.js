e4037a6df04078aacad0d5a327db9e23e6f73d73
rumman <us.rkhan129@gmail.com>
2021-02-11 01:10:21 -0500

    generated javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.skillwilltest.controller"},{"l":"com.skillwilltest.exception"},{"l":"com.skillwilltest.model"},{"l":"com.skillwilltest.repo"},{"l":"com.skillwilltest.service"},{"l":"com.skillwilltest.skillwilltest"}]
